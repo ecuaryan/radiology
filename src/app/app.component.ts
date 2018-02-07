b176e86c3b5c1a7a6d31b93ddc1338a344fa3cee
ecuaryan <ryanwillmore@gmail.com>
2018-02-07 01:41:18 -0700

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,15 +76,21 @@ export class AppComponent {
           },
           {
             'description': 'Spongiform',
+            'note': 'Spongiform: Composed predominantly (>50%) of small cystic spaces. Do not add further points for other categories.',
             'points': 0,
             'break': true
           },
           {
             'description': 'Mixed cystic and solid',
+            'note': 'Mixed cystic and solid: Assign points for predominant solid component.',
             'points': 1
           },
           {
             'description': 'Solid or almost completely solid',
+            'points': 2
+          },
+          {
+            'description': 'Cannot be determined because of calcification.',
             'points': 2
           }
         ]
@@ -96,19 +102,26 @@ export class AppComponent {
         'options': [
           {
             'description': 'Anechoic',
+            'note': 'Anechoic: Applies to cystic or almost completely cystic nodules.',
             'points': 0
           },
           {
             'description': 'Hyperechoic or isoechoic',
+            'note': 'Hyperechoic/isoechoic/hypoechoic: Compared to adjacent parenchyma.',
             'points': 1
           },
           {
             'description': 'Hypoechoic',
+            'note': 'Very hypoechoic: More hypoechoic than strap muscles.',
             'points': 2
           },
           {
             'description': 'Very hypoechoic',
             'points': 3
+          },
+          {
+            'description': 'Cannot be determined',
+            'points': 1
           }
         ]
       },
@@ -123,6 +136,7 @@ export class AppComponent {
           },
           {
             'description': 'Taller-than-wide',
+            'note': 'Taller-than-wide: Should be assessed on a transverse image with measurements parallel to sound beam for height and perpendicular to sound beam for width.',
             'points': 3
           }
         ]
@@ -142,11 +156,17 @@ export class AppComponent {
           },
           {
             'description': 'Lobulated or irregular',
+            'note': 'Lobulated: Protrusions into adjacent tissue.<br>Irregular: Jagged, spiculated, or sharp angles.',
             'points': 2
           },
           {
             'description': 'Extra-thyroidal extension',
+            'note': 'Extrathyroidal extension: Obvious invasion = malignancy.',
             'points': 3
+          },
+          {
+            'description': 'Cannot be determined',
+            'points': 0
           }
         ]
       },
@@ -157,18 +177,22 @@ export class AppComponent {
         'options': [
           {
             'description': 'None or large comet-tail artifacts',
+            'note': 'Large comet-tail artifacts: V-shaped, >1 mm, in cystic components.',
             'points': 0
           },
           {
             'description': 'Macrocalcifications',
+            'note': 'Macrocalcifications: Cause acoustic shadowing.',
             'points': 1
           },
           {
             'description': 'Peripheral (rim) calcifications',
+            'note': 'Peripheral: Complete or incomplete along margin.',
             'points': 2
           },
           {
             'description': 'Punctate echogenic foci',
+            'note': 'Punctate echogenic foci: May have small comet-tail artifacts.',
             'points': 3
           }
         ]
